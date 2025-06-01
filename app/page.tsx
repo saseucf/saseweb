0c8f358a840e4a5d5d97a81b95bf48728c9b0eb7
Eric George <ericgeo324@gmail.com>
2025-05-31 22:59:50 -0400

    working on home page carousel also need to fix hero

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,14 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
 import hero from "../public/hero.jpg";
 import Image from "next/image";
 export default function Home() {
@@ -22,34 +30,59 @@ export default function Home() {
                 <p className="text-2xl text-center pr-40 pl-40">
                     Since its founding in 2007, the Society of Asian Scientists and Engineers ​(SASE) has grown to a nationally recognized organization with 20,000 ​members worldwide, striving to help Asian heritage scientific and ​engineering professionals achieve their full potential. The University of ​Central Florida SASE Chapter was founded in 2020, shortly before the ​pandemic. With only 4 years under our belt, we have made tremendous ​strides towards the development of our members centered around core ​values of career, diversity, and service. Our events and programs not only ​advance professional pursuits, but also foster a supportive community that ​celebrates each and every member’s story.
                 </p>
+
+            </div>
+            <div className="flex flex-col grow items-center space-y-4 p-10">
                 <p className="text-4xl font-bold text-saseblue">Our Values</p>
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-2xl">Professional Description</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <p className="text-lg">
-                            We encourage members to ​leverage the experiences, ​knowledge, and skills gained ​through our organization to pursue ​their goals and aspirations.                        </p>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-2xl">Service</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <p className="text-lg">
-                            We commit ourselves to ​promoting service opportunities ​that enable our members to give ​back to the community and ​make a meaningful impact.</p>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-2xl">Diversity</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <p className="text-lg">
-                            We aim to empower our ​members by showcasing how ​their diverse backgrounds can ​broaden perspectives and ​inspire collaborative efforts.</p>
-                    </CardContent>
-                </Card>
+                <Carousel
+                    opts={{
+                        align: "start",
+                    }}
+                    className="w-full max-w-sm"
+                >
+                    <CarouselContent>
+
+                        <CarouselItem className="basis-1/2">
+                            <div className="p-1">
+                                <Card>
+                                    <CardHeader>
+                                        <CardTitle className="text-2xl">Professional Description</CardTitle>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <p className="text-lg">
+                                            We encourage members to ​leverage the experiences, ​knowledge, and skills gained ​through our organization to pursue ​their goals and aspirations.                        </p>
+                                    </CardContent>
+                                </Card>
+
+                            </div>
+                        </CarouselItem>
+                        <CarouselItem className="basis-1/2"><Card>
+                            <CardHeader>
+                                <CardTitle className="text-2xl">Service</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-lg">
+                                    We commit ourselves to ​promoting service opportunities ​that enable our members to give ​back to the community and ​make a meaningful impact.</p>
+                            </CardContent>
+                        </Card>
+                        </CarouselItem>
+                        <CarouselItem className="basis-1/2">
+
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle className="text-2xl">Diversity</CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    <p className="text-lg">
+                                        We aim to empower our ​members by showcasing how ​their diverse backgrounds can ​broaden perspectives and ​inspire collaborative efforts.</p>
+                                </CardContent>
+                            </Card>
+                        </CarouselItem>
+
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
             </div>
         </div>
     );
